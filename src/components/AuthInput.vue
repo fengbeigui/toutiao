445e8c447efe5e50904eb233ec1172f4c3b70929
fengbeigui <fbg0618@163.com>
2019-09-22 17:42:36 +0800

    添加输入框的状态

--- src/components/AuthInput.vue
+++ src/components/AuthInput.vue
@@ -1,43 +1,79 @@
 
 <template>
- <!-- /*  @input是输入框每次输入时候都会触发 */ -->
- <input type="text"
+  <!-- /*  @input是输入框每次输入时候都会触发 */ -->
+  <input
+    type="text"
     class="input"
+    :class="{
+        success: status === 'success',
+        error: status === 'error'
+    }"
     :placeholder="placeholder"
     :value="value"
     @input="handleInput"
- >
+    @change="handleChange"
+  />
 </template>
 
 <script>
 export default {
-    props:[
-        "placeholder",
-        "value",
-        "name",
-        "rule",
-    ],
+  data() {
+    return {
+      status: ""
+    };
+  },
+// rule传递过来的正则表达式
+  props: [
+      "placeholder",
+      "value",
+      "name", 
+      "rule"],
 
-    methods:{
-        //每次输入框输入值时都会触发
-        handleInput(event){
-            this.$emit("input",event.target.value)
+  methods: {
+    //每次输入框输入值时都会触发
+    handleInput(event) {
+      //this.$emit("input", event.target.value);
+      const {value} = event.target;
+
+      //触发父组件的input事件，返回输入框的值
+      this.$emit("input",value);
+
+      //如果符合正则就位success,反之是error
+      if(this.rule){
+          if(this.rule.test(value)){
+              this.status = "success";
+          }else{
+              this.status = "error";
+          }
+      }
+    },
+    //输入框失去焦点时候触发
+    handleChange(){
+        if(this.err_message){
+            alert(this.err_message)
         }
     }
-}
+  }
+};
 </script>
 
 <style scoped lang="less">
 //scoped 作用域样式
 //lang声明样式的类型
-.input{
-    width:100%;
-    height:38/360*100vw;
-    padding:20px;
-    box-sizing:border-box;
-    background:#fff;
-    border:none;
-    border-bottom:1px #666 solid;
-    outline:none;
+.input {
+  width: 100%;
+  height: 38/360 * 100vw;
+  padding: 20px;
+  box-sizing: border-box;
+  background: #fff;
+  border: none;
+  border-bottom: 2px #666 solid;
+  outline: none;
+}
+.success{
+    border-color: blue;
+}
+.error{
+    border-color: red;
 }
 </style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,7 +9,14 @@
       <span class="iconfont iconnew"></span>
     </div>
     <!-- 用户名密码输入框 -->
-    <AuthInput placeholder="手机号码" :value="form.username" @input="handleUsername"></AuthInput>
+    <AuthInput 
+    placeholder="手机号码" 
+    :value="form.username" 
+    @input="handleUsername"
+
+    :rule="/^1[0-9]{4,10}$/"
+    err_message="手机号码格式不正确"
+    ></AuthInput>
 
     <button @click="handleSubmit">登录按钮</button>
   </div>
