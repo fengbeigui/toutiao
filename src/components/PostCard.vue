23cbd48330cc7e8e05536cf0145c50f0cb50d944
fengbeigui <fbg0618@163.com>
2019-09-26 20:11:24 +0800

    多张图片的文章列表布局方式

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,27 +1,48 @@
 <template>
-  <div class="card">
-    <!-- 左侧文字 -->
-    <div class="card-left">
-      <div class="post-title">
-          {{post.title}}
+  <div>
+      <!-- 单张图片显示的布局 -->
+    <div class="card" v-if="post.cover.length > 0 && post.cover.length < 3">
+      <!-- 左侧文字 -->
+      <div class="card-left">
+        <div class="post-title">
+            {{post.title}}
+        </div>
+        <p class="post-info">
+          <span>{{post.user.nickname}}</span>
+          <span>{{post.comment_length}}跟帖</span>
+        </p>
       </div>
-      <p class="post-info">
-        <span>{{post.user.nickname}}</span>
-        <span>{{post.comment_length}}跟帖</span>
-      </p>
+      <div class="card-img">
+        <img :src="post.cover[0].url" alt />
+      </div>
     </div>
-    <div class="card-img">
-      <img
-        :src="post.cover[0].url"
-        alt
-      />
+
+    <!-- 三张图片显示的布局 -->
+    <div class="img-cart" v-if="post.cover.length >= 3">
+        <div class="post-title">
+            {{post.title}}
+        </div>
+        <div class="img-list">
+            <img
+            v-for="(item,index) in post.cover"
+            :key="index"
+            :src="item.url"
+            v-if="index < 3"
+            >
+        </div>
+            <p class="post-info">
+                <span>{{post.user.nickname}}</span>
+                <span>{{post.comment_length}}跟帖</span>
+            </p>
+
     </div>
+
   </div>
 </template>
 
 <script>
 export default {
-    props:['post']
+  props: ["post"]
 };
 </script>
 
@@ -34,7 +55,6 @@ export default {
   border-bottom: 1px solid #ccc;
 
   .card-left {
-      
     flex: 1;
 
     .post-title {
@@ -46,20 +66,49 @@ export default {
       -webkit-line-clamp: 2;
       overflow: hidden;
     }
+    .post-info {
+      font-size: 12px;
+      color: #999;
+    }
+  }
+  .card-img {
+    img {
+      display: block;
+      width: 120/360 * 100vw;
+      height: 80/360 * 100vw;
+      object-fit: cover;
+    }
+  }
+}
+.img-cart{
+    padding:20px 10px;
+    border-bottom:1px #ccc solid;
+
+    .post-title{
+        font-size:14px;
+        line-height:1.5;
+        margin-bottom:5px;
+        display:-webkt-box;
+        -webkit-box-orient:vertical;
+        -webkit-line-clamp:2;
+        overflow: hidden;
+    }
+    .img-list{
+        display:flex;
+        justify-content:space-between;
+        margin-bottom:5px;
+
+        img{
+            display: block;
+            width: 33%;
+            height:80/360*100vw;
+            object-fit:cover;
+        }
+    }
     .post-info{
         font-size:12px;
         color:#999;
-
     }
-  }
-  .card-img{
-      img{
-          display: block;
-          width: 120/360*100vw;
-          height: 80/360*100vw;
-          object-fit: cover;
 
-      }
-  }
 }
 </style>
