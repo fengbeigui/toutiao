09b1e502ba5a293a4b00968b00e3ada765dd7db0
fengbeigui <fbg0618@163.com>
2019-09-30 02:09:42 +0800

    跟帖页面导入公共头部

--- src/pages/postcomment/PostComment.vue
+++ src/pages/postcomment/PostComment.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-      123
+      <!-- 头部组件 -->
+      <HeaderNormal title="精彩跟帖"/>
   </div>
 </template>
 
 <script>
+import HeaderNormal from "@/components/HeaderNormal"
 export default {
-
+        //导入一定要注册组件
+        components:{
+            HeaderNormal
+        }
 }
 </script>
 
